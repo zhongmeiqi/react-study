3ccba9558bf66a71f5dcd49651138c914b543364
zhongmeiqi <wxzhongmeiqi@gf.com.cn>
2023-05-07 22:50:28 +0800

    feat:组件状态初探

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,19 +1,10 @@
-import ReportButton from "./component/ReportButton";
+import Counter from "./component/Counter";
 
-const obj = {
-  a: 1,
-};
-
-obj[Symbol.iterator] = function* () {
-  for (let prop in obj) {
-    yield [prop, obj[prop]];
-  }
-};
 function App() {
   return (
     // 这个div不是真正的dom元素，而是react元素
     <div>
-    <ReportButton/>
+      <Counter/>
     </div>
     // 如果是大写开头的 React默认为组件
   );
---
+++ my-first-react-project/src/component/Counter/index.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+
+// Counter组件可以用在任何你需要计数的地方
+
+export default function Counter(){
+/* 
+    count的值变动了，count虽然是Counter内部的数据，但是他凭什么能够让React去重新渲染组件呢
+    我们要知道 函数组件内部的数据有很多种，那我们要选能够通知React去重新渲染组件的状态
+    那么我们需要在内部创造出一种数据，这种数据可以和React产生直接关联，从而使得当组件状态变化时，React可以重新更新页面
+*/
+let [count,setCount] = useState(0);//调用React的useState函数去生成了一个数据
+const increase = ()=>{
+    // 这里我们虽然改变了count的值，但是没有让Counter组件重新执行 count+=1
+    setCount(pre=>pre+1) // 只要你调用了setCount函数去对数据产生了变动，那么当前使用了该状态的React组件就会重新渲染
+
+  /*   function setCount(newValue){
+        count = newValue;
+        reRenderThisCOmponent() 重新渲染该组件
+    } */
+}
+
+const decrease = ()=>{
+    setCount(pre=>pre-1)
+
+}
+
+window.getCount=()=>{
+    console.log("count",count)
+}
+/* 
+    我们最终执行的代码是通过Babel编译以后的代码
+    React.createElement("span",{},count) ---> 调用document上面的一些方法去改变真实的dom的显示状态
+    count---> 0 如果我们想要页面里发生一点显示效果的变化，我们得让React.createElement这段代码重复执行
+    渲染 === 执行 / 重新渲染 === 函数组件的重新执行
+*/
+    return (
+        <div>
+            <button onClick={increase}>+</button>
+            <span>{count}</span>
+            <button onClick={decrease}>-</button>
+
+        </div>
+    )
+}
