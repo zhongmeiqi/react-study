e1a04a042be6f671378cb435e417f1ce75e22702
zhongmeiqi <wxzhongmeiqi@gf.com.cn>
2023-05-08 19:57:25 +0800

    feat：组件状态useState

--- my-first-react-project/src/component/Counter/index.jsx
+++ my-first-react-project/src/component/Counter/index.jsx
@@ -9,10 +9,27 @@ export default function Counter(){
     我们要知道 函数组件内部的数据有很多种，那我们要选能够通知React去重新渲染组件的状态
     那么我们需要在内部创造出一种数据，这种数据可以和React产生直接关联，从而使得当组件状态变化时，React可以重新更新页面
 */
+
+/* function useState(){
+    if(isInit){
+        // 第一次调用useState 我们就可以理解为初始化
+    }else{
+
+    }
+} */
+// 这段代码是百分之百执行的，但是useState内部的初始化工作不会再做了
+
 let [count,setCount] = useState(0);//调用React的useState函数去生成了一个数据
 const increase = ()=>{
-    // 这里我们虽然改变了count的值，但是没有让Counter组件重新执行 count+=1
+    // 这里我们虽然改变了count的值，但是没有让Counter组件重新执行 count+=1 
+     
+    // 这个count的值是异步更新的
     setCount(pre=>pre+1) // 只要你调用了setCount函数去对数据产生了变动，那么当前使用了该状态的React组件就会重新渲染
+    setCount(pre=>pre+1)
+    console.log(11)
+    // 增加 每一次都在上一次的值上面+1,
+    /* setCount(count+1)
+    setCount(count+1) */
 
   /*   function setCount(newValue){
         count = newValue;
